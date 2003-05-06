61225138244df8f4ed6188cfe0b10b9037c99e97
George Schlossnagle <gschlossnagle@php.net>
2003-05-06 03:06:26 +0000

    now regex is _really_ bad

--- slides/hpp/bad_regex.php
+++ slides/hpp/bad_regex.php
@@ -4,19 +4,19 @@ $UNSAFE_HTML = array(
 	"!vbscri?pt\s*:!is",
 	"!<\s*embed.*swf!is",
 	"!vbscri?pt\s*:!is",
-	"!<[^>]*[^a-z]onabort\s*=!is",
-	"!<[^>]*[^a-z]onblur\s*=!is",
-	"!<[^>]*[^a-z]onchange\s*=!is",
-	"!<[^>]*[^a-z]onfocus\s*=!is",
-	"!<[^>]*[^a-z]onmouseout\s*=!is",
-	"!<[^>]*[^a-z]onmouseover\s*=!is",
-	"!<[^>]*[^a-z]onload\s*=!is",
-	"!<[^>]*[^a-z]onreset\s*=!is",
-	"!<[^>]*[^a-z]onselect\s*=!is",
-	"!<[^>]*[^a-z]onsubmit\s*=!is",
-	"!<[^>]*[^a-z]onunload\s*=!is",
-	"!<[^>]*[^a-z]onerror\s*=!is",
-	"!<[^>]*[^a-z]onclick\s*=!is"
+	"!<.*[^a-z]onabort\s*=!is",
+	"!<.*[^a-z]onblur\s*=!is",
+	"!<.*[^a-z]onchange\s*=!is",
+	"!<.*[^a-z]onfocus\s*=!is",
+	"!<.*[^a-z]onmouseout\s*=!is",
+	"!<.*[^a-z]onmouseover\s*=!is",
+	"!<.*[^a-z]onload\s*=!is",
+	"!<.*[^a-z]onreset\s*=!is",
+	"!<.*[^a-z]onselect\s*=!is",
+	"!<.*[^a-z]onsubmit\s*=!is",
+	"!<.*[^a-z]onunload\s*=!is",
+	"!<.*[^a-z]onerror\s*=!is",
+	"!<.*[^a-z]onclick\s*=!is"
 );
 
 function unsafe_html($html) {
