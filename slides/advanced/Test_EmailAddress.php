d73fe05d16939e3cc11556ffbac4a6fc860455e1
Sterling Hughes <sterling@php.net>
2003-05-13 03:53:12 +0000

    fixes

--- slides/advanced/Test_EmailAddress.php
+++ slides/advanced/Test_EmailAddress.php
@@ -3,14 +3,19 @@ require_once('PHPUnit.php');
 require_once('EmailAddress.php');
 
 class Test_EmailAddress extends PHPUnit_TestCase {
+	private $address = 'sterling@php.net';
+	
 	function Test_UserPart() {
-		$email = new EmailAddress('sterling@php.net');
-		self::assertTrue($email->user == 'sterling');
+		$email = new EmailAddress($this->address);
+		self::assertTrue(
+			$email->user == 'sterling',
+		    "{$email->host} != {$this->address}\n"
+		);
 	}
 
 	function Test_ServerPart() {
-		$email = new EmailAddress('sterling@php.net');
-		self::assertTrue($email->host == 'php.net');
+		$email = new EmailAddress($this->address);
+		self::assertEquals($email->host, 'php.net');
 	}
 }
 
@@ -21,5 +26,5 @@ $suite->addTest(
 );
 
 $result = PHPUnit::run($suite);
-echo $result->toString();
+echo nl2br($result->toString());
 ?>
