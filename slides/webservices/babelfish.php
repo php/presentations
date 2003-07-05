fe70131de3e84e8efa90b3748c88e2b044cb4e91
Sterling Hughes <sterling@php.net>
2003-07-05 22:19:23 +0000

    add some new files and examples

---
+++ slides/webservices/babelfish.php
@@ -0,0 +1,10 @@
+<?php
+require_once('SOAP/Client.php');
+
+$wsdlurl = 'BabelFishService.wsdl';
+
+$WSDL = new SOAP_WSDL($wsdlurl);
+$babel = $WSDL->getProxy();
+
+echo $babel->BabelFish('de_en', 'Unser erstes Beispiel');
+?>
