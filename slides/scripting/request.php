30d4e34ff0203181a759b29895b6b19fc6592de4
Sterling Hughes <sterling@php.net>
2003-07-20 15:16:28 +0000

    Add/modify some slides from my Miraflores presentation

--- slides/scripting/request.php
+++ slides/scripting/request.php
@@ -1,22 +1,13 @@
-#!/usr/bin/php4
 <?php
 
 if ($argc < 2) {
     die ("You must specify a URL\n");
 }
 
-
-$port = 80;
-if ($argc > 2) {
-    $port = $argv[2];
-}
-
 $url = $argv[1];
-
 if (!preg_match ("@^\S+\://@", $url)) {
     die ("You must specify a URL identifier\n");
 }
 
-/* Later versions of php can use file_get_contents ($url) */
-print implode ("", file($url));
+print file_get_contents($url);
 ?>
