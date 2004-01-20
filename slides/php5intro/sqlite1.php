b8f9734f199752a54ce371605f3bdfadf3f090c3
Rasmus Lerdorf <rasmus@php.net>
2004-01-20 18:39:00 +0000

    Have been a bit lax in adding presentations to CVS

--- slides/php5intro/sqlite1.php
+++ slides/php5intro/sqlite1.php
@@ -1,12 +1,11 @@
 <?php
-$db = sqlite_open(':memory:', 0666, $sqliteerror);
+@exec('rm /tmp/lb.db');
+$db = sqlite_open('/tmp/lb.db', 0666, $sqliteerror);
 
 if ($db) {
 
-  sqlite_query($db,
-	       'CREATE TABLE foo (bar varchar(10))');
-  sqlite_query($db,
-	       "INSERT INTO foo VALUES ('fnord')");
+  sqlite_query($db, 'CREATE TABLE foo (bar varchar(10))');
+  sqlite_query($db, "INSERT INTO foo VALUES ('fnord')");
 
   $result = sqlite_query($db,'select bar from foo');
   $record = sqlite_fetch_array($result);
