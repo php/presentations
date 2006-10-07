0720bfbce5913538345793bfc6f8a5edd6236731
Tobias Schlitt <toby@php.net>
2006-10-07 15:48:50 +0000

    - Added talk for PHP Weekender tomorrow.

---
+++ slides/php-weekender/debugging-xdebug-using-1.php
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="iso-8859-1"?>
+<slide fontsize="3em">
+	<title>Debugging - Xdebug</title>
+
+	<blurb></blurb>
+	<blurb>Important setings are (standard values shown):</blurb>
+	<list>
+	 <bullet>xdebug.auto_trace="off"</bullet>
+	 <bullet>xdebug.collect_includes="on"</bullet>
+	 <bullet>xdebug.collect_params="off"</bullet>
+	 <bullet>xdebug.collect_return="off"</bullet>
+	 <bullet>xdebug.default_enable="on"</bullet>
+	 <bullet>xdebug.show_local_vars=0</bullet>
+	 <bullet>xdebug.max_nesting_level=64</bullet>
+	</list>
+</slide>
