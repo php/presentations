ef66549d0a8d1bebf5692c1e836a321a5d7c402d
Rasmus Lerdorf <rasmus@php.net>
2011-05-27 20:26:56 +0000

    2 new YQL slides

---
+++ slides/intro/yql.php
@@ -0,0 +1,23 @@
+<?php
+include 'secrets.inc';
+define('MINSZ',10);
+function cache($url) {
+  $tmp = '/tmp/'.md5($url);
+  if(file_exists($tmp)) $st = stat($tmp);
+  if(!$st || $st && ($st['size']<MINSZ || $st['mtime']<(time()-7200))) {
+    if($st && $st['size']>=MINSZ) touch($tmp);  // Keep re-using entry
+    $stream = fopen($url,'r');                  // until new is ready
+    $tmpf = tempnam('/tmp','YWS');
+    file_put_contents($tmpf, $stream);
+    fclose($stream);
+    rename($tmpf, $tmp);
+  }
+  return $tmp;
+}
+
+function yql($q, $fmt='xml') {
+  $yql = "http://query.yahooapis.com/v1/public/yql?q=";
+  $env = 'store://datatables.org/alltableswithkeys';
+  $x = simplexml_load_file(cache($yql.urlencode($q)."&format=$fmt&env=".urlencode($env)));
+  return $x;
+}
