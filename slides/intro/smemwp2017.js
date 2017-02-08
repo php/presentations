399ecf318d810ef2f2f56e818e1d1512b7d91b65
Rasmus Lerdorf <rasmus@php.net>
2017-02-07 23:28:26 -0800

    Tweakers talk

---
+++ slides/intro/smemwp2017.js
@@ -0,0 +1,102 @@
+window["render_smemwp"] = function() {
+    $('#smemwp_container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Wordpress 4.7.2 Memory',
+			style: { fontSize: '40px' }
+        },
+        subtitle: {
+            text: 'http://wordpress/2017/02/05/hello-world/',
+			style: { fontSize: '20px' }
+        },
+        xAxis: [{
+            categories: [ 'PHP 5.3', 'PHP 5.4', 'PHP 5.5', 'PHP 5.6', 'PHP 7.0', 'PHP 7.1', 'PHP 7.2', 'PHP 7.2 FDO' ],
+            labels: {
+                style: { color: '#000', fontSize: '20px' }
+            }
+        }],
+        yAxis: [{
+            min: 0,
+			showEmpty: false,
+            title: {
+                text: 'M',
+				style: { fontSize: '25px' }
+            },
+			labels: {
+				style: { color: '#111', fontSize: '15px' }
+			}
+        }],
+        tooltip: {
+			enabled: false
+        },
+		legend: {
+			title: { text: 'Type' }
+		},
+        plotOptions: {
+            column: {
+                pointPadding: 0.01,
+                groupPadding: 0.05,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'USS - Unique Set Size',
+			yAxis: 0,
+            data: [110.4, 87.3, 75.1, 70.6, 10.3,10.5,10.2,10.1],
+            dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#000',
+                align: 'right',
+                x: 4,
+                y: 5,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif',
+                },
+                format: '{point.y:.1f}'
+			},
+			visible: false
+        }, {
+            name: 'PSS - Proportional Set Size',
+			yAxis: 0,
+            data: [139.7,111.7,99.6,93.9,15.3,15.4,15.2,15.1],
+            dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#fff',
+                align: 'right',
+                x: 4,
+                y: 5,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif',
+                },
+                format: '{point.y:.1f}'
+			},
+			visible: true
+
+        }, {
+            name: 'RSS - Resident Set Size',
+			yAxis: 0,
+            data: [427.1,356.6,341.9,325,92.3,92.6,92.2,92],
+            dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#000',
+                align: 'right',
+                x: 4,
+                y: 5,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif',
+                },
+                format: '{point.y:.1f}'
+			},
+			visible: false
+        }],
+		credits: false
+    });
+}
---
+++ slides/intro/wp2017.js
@@ -0,0 +1,171 @@
+window["render_wpbench"] = function() {
+    $('#wpbench_container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Wordpress 4.7.2',
+			style: { fontSize: '40px' }
+        },
+        subtitle: {
+            text: 'http://wordpress/2017/02/05/hello-world/',
+			style: { fontSize: '20px' }
+        },
+        xAxis: [{
+            categories: [ 'PHP 5.3', 'PHP 5.4', 'PHP 5.5', 'PHP 5.6', 'PHP 7.0', 'PHP 7.1', 'PHP 7.2', 'PHP 7.2 FDO' ],
+            labels: {
+                style: { color: '#000', fontSize: '20px' }
+            }
+        }],
+        yAxis: [{
+            min: 0,
+			showEmpty: false,
+            title: {
+                text: 'Req/sec',
+				style: { fontSize: '25px' }
+            },
+			labels: {
+				style: { color: '#111', fontSize: '15px' }
+			}
+        }, {
+			min: 0,
+			showEmpty: false,
+			title: {
+				text: 'Latency (ms)',
+				style: { fontSize: '25px' }
+			},
+            labels: {
+				format: '{value}ms',
+				style: { color: '#111', fontSize: '15px' }
+            },
+			opposite: true
+		}],
+        tooltip: {
+			enabled: false
+        },
+		legend: {
+			title: { text: 'Concurrent clients' }
+		},
+        plotOptions: {
+            column: {
+                pointPadding: 0.01,
+                groupPadding: 0.05,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'r/s @ 10',
+			yAxis: 0,
+            data: [81.17,102.23,101.52,106.27,204.06,208.35,210.53,216.27],
+            dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#000',
+                align: 'right',
+                x: 4,
+                y: 5,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif',
+                },
+                format: '{point.y:.0f}'
+			},
+			visible: false
+        }, {
+            name: 'r/s @ 20',
+			yAxis: 0,
+            data: [81.36,101.31,101.94,106.31,205.52,208.43,210.88,214.58],
+            dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#fff',
+                align: 'right',
+                x: 4,
+                y: 5,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif',
+                },
+                format: '{point.y:.0f}'
+			},
+			visible: true
+
+        }, {
+            name: 'r/s @ 40',
+			yAxis: 0,
+            data: [81.69,101.55,101.72,105.58,204.43,209.04,210.45,214.66],
+            dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#000',
+                align: 'right',
+                x: 4,
+                y: 5,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif',
+                },
+                format: '{point.y:.0f}'
+			},
+			visible: false
+        }, {
+            name: 'latency @ 10',
+			yAxis: 1,
+			data: [120,100,100,90,50,50,50,50],
+			tooltip: { valueSuffix: 'ms' },
+            dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#000',
+                align: 'right',
+                x: 4,
+                y: 5,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif',
+                },
+                format: '{point.y:.0f}'
+			},
+			visible: false
+		}, {
+            name: 'latency @ 20',
+			yAxis: 1,
+			data: [250,200,200,190,100,100,100,90],
+			tooltip: { valueSuffix: 'ms' },
+            dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#000',
+                align: 'right',
+                x: 4,
+                y: 5,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif',
+                },
+                format: '{point.y:.0f}'
+			},
+			visible: false
+		}, {
+            name: 'latency @ 40',
+			yAxis: 1,
+			data: [490,390,390,380,200,190,190,190],
+			tooltip: { valueSuffix: 'ms' },
+            dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#000',
+                align: 'right',
+                x: 4,
+                y: 5,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif',
+                },
+                format: '{point.y:.0f}'
+			},
+			visible: false
+		}],
+		credits: false
+    });
+}
