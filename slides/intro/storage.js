2d66074bd6a4318c10b557eaac14cbfbc5b0b599
Rasmus Lerdorf <rasmus@lerdorf.com>
2021-09-29 06:08:24 -0700

    Tweaks

--- slides/intro/storage.js
+++ slides/intro/storage.js
@@ -1,14 +1,16 @@
 window["render_storage"] = function() {
     $('#storage_container').highcharts({
         chart: {
-            type: 'column'
+            type: 'column',
+            zoomKey: 'alt',
+            zoomType: 'y'
         },
         title: {
             text: 'KV storage access times',
 			style: { fontSize: '40px' }
         },
         subtitle: {
-            text: '',
+            text: '70M include file, 131,072 rows, keys 8-48 chars, vals 16-1023 chars',
 			style: { fontSize: '20px' }
         },
         xAxis: [{
@@ -31,7 +33,7 @@ window["render_storage"] = function() {
 			min: 0,
 			showEmpty: false,
 			title: {
-				text: 'Initial (ms)',
+				text: 'Time (ms)',
 				style: { fontSize: '25px' }
 			},
             labels: {
@@ -94,6 +96,25 @@ window["render_storage"] = function() {
             name: 'Initial',
 			yAxis: 0,
 			data: [0.01237,0.00212,0.19347,0.36944,0.18185,0.29536,0.3215,0.55842],
+			tooltip: { valueSuffix: 'ms' },
+            dataLabels: {
+                enabled: false,
+                rotation: -90,
+                color: '#000',
+                align: 'right',
+                x: 4,
+                y: 5,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif',
+                },
+                format: '{point.y:.0f}'
+			},
+			visible: false
+		}, {
+            name: 'Write',
+			yAxis: 0,
+			data: [0.90781,0.00255,0.03527,0.09587,0.03272,0.13062,5.07599,34.00938],
 			tooltip: { valueSuffix: 'ms' },
             dataLabels: {
                 enabled: false,
